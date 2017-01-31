e6b75cab07cf89ffe7ce751b17b585642c7668a7
Thorsten Scherler <scherler@gmail.com>
2017-01-31 17:33:46 +0100

    Update writeTest.groovy

--- vars/writeTest.groovy
+++ vars/writeTest.groovy
@@ -4,9 +4,9 @@ def call() {
     def stringWriter = new StringWriter()
     def testResultBuilder = new MarkupBuilder(stringWriter)
     testResultBuilder.testsuites {
-        
+        testsuite(name: 'JUnitXmlReporter')
     }
     String xml = stringWriter.toString()
 println xml
-    return "xml"
+    return xml
 }
