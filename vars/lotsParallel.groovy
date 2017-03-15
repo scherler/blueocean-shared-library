603a6cc758134bf74673c6bf19d0d8e6914780ec
Thorsten Scherler <scherler@gmail.com>
2017-03-15 20:02:44 +0100

    Update lotsParallel.groovy

--- vars/lotsParallel.groovy
+++ vars/lotsParallel.groovy
@@ -1,8 +1,11 @@
+def build_tasks = [:]
 def call(max = 110) {
-parallel 
+
 for (ii = 0; ii < max; ii++) {
-        "parallel ${ii}": {
+        build_tasks["parallel-${ii}"] = {
           println "Number ${ii}"
-        },
+        }
     }   
 }
+
+parallel build_tasks
